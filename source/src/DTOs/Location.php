fe5025773a84a1c00d68e733392aeff0c189a521
Philipp Just <Philipp.Just@gmail.com>
2020-11-07 21:22:39 +0100

    Use isHome in Mail

--- source/src/DTOs/Location.php
+++ source/src/DTOs/Location.php
@@ -4,13 +4,15 @@ namespace OpenAPIServer\DTOs;
 
 class Location
 {
-    
+
     public ?Address $address;
+    public ?bool $isHome;
     public Coordinates $coordinates;
-    
-    public function __construct(?Address $address, Coordinates $coordinates)
+
+    public function __construct(?Address $address, Coordinates $coordinates, ?bool $isHome)
     {
         $this->address = $address;
         $this->coordinates = $coordinates;
+        $this->isHome = $isHome;
     }
 }
